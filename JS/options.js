ad70f9e425b9ac68fe6bb47f83f769a00a909ef9
Lumian1711 <Lumian1711>
2025-04-11 18:38:57 -0600

    Api _ Publicaciones

--- JS/options.js
+++ JS/options.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(() => {
-        document.getElementById("loading-screen").style.display = "none"; // Oculta la pantalla de carga
-        document.body.classList.remove("loading-bg"); // Quita el fondo inicial
-        document.body.classList.add("default-bg"); // Agrega el fondo normal
-        document.getElementById("content").classList.remove("hidden"); // Muestra el contenido
-    }, 3000); // Espera 3 segundos antes de mostrar el contenido
+        document.getElementById("loading-screen").style.display = "none"; 
+        document.body.classList.remove("loading-bg");
+        document.body.classList.add("default-bg");
+        document.getElementById("content").classList.remove("hidden"); 
+    }, 3000); 
 });
 function updateVolumeValue(value) {
     document.getElementById("volumeValue").textContent = value;
